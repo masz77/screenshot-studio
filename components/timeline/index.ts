7aab24346f85aef38a7feeeae80e360d1b519c96
sam <76036008+masz77@users.noreply.github.com>
2026-04-16 21:25:13 +0700

    refactor: remove 4 unused legacy timeline components
    TimelinePlayhead, TimelineRuler, TimelineTrack, and KeyframeMarker
    were from the old keyframe-based timeline, replaced by clip-based system.
    Clean barrel exports in index.ts.

--- components/timeline/index.ts
+++ components/timeline/index.ts
@@ -1,9 +1,5 @@
 // Timeline components barrel export
 export { TimelineEditor } from './TimelineEditor';
 export { TimelineControls } from './TimelineControls';
-export { TimelineRuler } from './TimelineRuler';
-export { TimelineTrack } from './TimelineTrack';
-export { TimelinePlayhead } from './TimelinePlayhead';
-export { KeyframeMarker } from './KeyframeMarker';
 export { AnimationPresetGallery } from './AnimationPresetGallery';
 export { useTimelinePlayback } from './hooks/useTimelinePlayback';
